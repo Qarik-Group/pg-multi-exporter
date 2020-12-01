886865a72c80a339b199a259c7957240d68e447e
rkoster <rkoster@starkandwayne.com>
2020-12-01 16:59:05 +0100

    Telegraf to influxdb works

--- images/postgres/enable-stats.sh
+++ images/postgres/enable-stats.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-echo "shared_preload_libraries = 'pg_stat_statements'" >> $PGDATA/postgresql.conf
-echo "pg_stat_statements.max = 10000" >> $PGDATA/postgresql.conf
-echo "pg_stat_statements.track = all" >> $PGDATA/postgresql.conf
-
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+     CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+EOSQL
