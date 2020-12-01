55577589540be80c83672f9a85adac286a5d150f
Justin Carter <justin@misthos.io>
2020-12-01 14:20:17 +0100

    Rename run.sh -> run via ytt --file-mark

--- pre-start.sh
+++ pre-start.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-ytt -f /templates --file-mark '**/*.sh:type=text-template' --output-files /etc
+ytt -f /templates \
+  --file-mark '**/*.sh:type=text-template' \
+  --file-mark 'services.d/postgres_exporter/run.sh:path=services.d/postgres_exporter/run' --output-files /etc
