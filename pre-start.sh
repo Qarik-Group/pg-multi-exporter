362418fd5b492dd930957c0c33056385e1c37228
Justin Carter <justin@misthos.io>
2020-12-07 15:55:42 +0100

    Move telegraf process into deliverable

--- pre-start.sh
+++ pre-start.sh
@@ -1,6 +1,15 @@
-#!/bin/sh
+#!/usr/bin/with-contenv sh
+
+cat <<EOF > /templates/inputs.yml
+#@data/values
+---
+influxdb:
+  endpoint: "${INFLUXDB_ENDPOINT}"
+EOF
 
 ytt -f /templates \
   --file-mark '**/*.sh:type=text-template' \
+  --file-mark '**/*.conf:type=text-template' \
   --file-mark 'services.d/postgres_exporter/run.sh:path=services.d/postgres_exporter/run' \
+  --file-mark 'services.d/telegraf/run.sh:path=services.d/telegraf/run' \
   --output-files /etc
