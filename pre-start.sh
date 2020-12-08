9052f42b25f4d77a51a3ab7475cfa16d69806ac3
ramonskie <ramonmakkelie@gmail.com>
2020-12-08 14:53:36 +0100

    refactor stackdriver and dynamic files via .env

--- pre-start.sh
+++ pre-start.sh
@@ -14,6 +14,7 @@ gcp:
 ytt -f /templates \
   --file-mark '**/*.sh:type=text-template' \
   --file-mark '**/*.conf:type=text-template' \
+  --file-mark '**/*.json:type=text-template' \
   --file-mark 'services.d/postgres_exporter/run.sh:path=services.d/postgres_exporter/run' \
   --file-mark 'services.d/stackdriver_exporter/run.sh:path=services.d/stackdriver_exporter/run' \
   --file-mark 'services.d/telegraf/run.sh:path=services.d/telegraf/run' \
