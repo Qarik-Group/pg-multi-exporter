4cd90b859295220e252f1cb7f528e42b32151482
rkoster <rkoster@starkandwayne.com>
2020-12-10 19:11:53 +0100

    Finish cloudwatch support

--- templates/services.d/telegraf/run.sh
+++ templates/services.d/telegraf/run.sh
@@ -2,8 +2,4 @@
 
 export GOOGLE_APPLICATION_CREDENTIALS=/etc/telegraf/stackdriver_service_account.json
 
-export AWS_DEFAULT_REGION=""
-export AWS_ACCESS_KEY_ID=""
-export AWS_SECRET_ACCESS_KEY=""
-
 telegraf
