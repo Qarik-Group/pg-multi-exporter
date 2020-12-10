fe517246bcc267c9ef3e264b2589dedd3086842e
ramonskie <ramonmakkelie@gmail.com>
2020-12-10 13:15:56 +0100

    wip aws cloudwatch

--- templates/services.d/telegraf/run.sh
+++ templates/services.d/telegraf/run.sh
@@ -2,4 +2,8 @@
 
 export GOOGLE_APPLICATION_CREDENTIALS=/etc/telegraf/stackdriver_service_account.json
 
+export AWS_DEFAULT_REGION=""
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY=""
+
 telegraf
