aa6bea80b30d523657e69ab25f621b7ef30f3892
ramonskie <ramonmakkelie@gmail.com>
2020-12-14 15:13:49 +0100

    use different iaas config data load in ytt so we can use ops files

--- pre-start.sh
+++ pre-start.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-ytt --data-value-yaml "config=$(cat /config.yml)" -f /templates \
+if [ -f /config-iaas.yml ]; then
+  iaas_arg='--data-value-yaml "iaas_config=$(cat /config-iaas.yml)"'
+fi
+
+ytt -f /templates \
+  --data-value-yaml "config=$(cat /config.yml)" \
+  ${iaas_arg} \
   --file-mark '**/*.sh:type=text-template' \
   --file-mark '**/*.conf:type=text-template' \
   --file-mark '**/*.json:type=text-template' \
