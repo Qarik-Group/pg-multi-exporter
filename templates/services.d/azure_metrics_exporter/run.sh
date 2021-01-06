3ad5518fbbc03cb3a0dc65b29635d0c485870819
ramonskie <ramonmakkelie@gmail.com>
2021-01-06 11:10:05 +0100

    do not add azure when not configured in config-iaas.yml

--- templates/services.d/azure_metrics_exporter/run.sh
+++ templates/services.d/azure_metrics_exporter/run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 (@ load("@ytt:data", "data") @)
 
+(@ if hasattr(data.values.iaas_config, "azure"): @)
+
 export AZURE_TENANT_ID="(@= data.values.iaas_config.azure.tenant_id @)"
 export AZURE_CLIENT_ID="(@= data.values.iaas_config.azure.client_id @)"
 export AZURE_CLIENT_SECRET="(@= data.values.iaas_config.azure.client_secret @)"
 
-azure-metrics-exporter --debug
+azure-metrics-exporter
+
+(@ else: @)
+s6-svc -dXO /var/run/s6/services/azure_metrics_exporter
+(@ end @)
