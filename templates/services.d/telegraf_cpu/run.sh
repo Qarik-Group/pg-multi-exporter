b438f2a33b5fc4ced617b4aa9a03ec051b73ac74
ramonskie <ramonmakkelie@gmail.com>
2021-01-11 11:50:17 +0100

    disable extra telegraf proccess if gcp is not used

--- templates/services.d/telegraf_cpu/run.sh
+++ templates/services.d/telegraf_cpu/run.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+(@ load("@ytt:data", "data") @)
+(@ if hasattr(data.values.iaas_config, "gcp"): @)
 
 export GOOGLE_APPLICATION_CREDENTIALS=/etc/telegraf/stackdriver_service_account.json
 
 telegraf --config /etc/telegraf/telegraf_cpu.conf
+
+(@ else: @)
+s6-svc -dXO /var/run/s6/services/telegraf_cpu
+(@ end @)
