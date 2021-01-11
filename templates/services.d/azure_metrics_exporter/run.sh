29e9012c89a89cafbc7f9de4e169a960c36144ed
ramonskie <ramonmakkelie@gmail.com>
2021-01-11 12:08:17 +0100

    enable caching for azure

--- templates/services.d/azure_metrics_exporter/run.sh
+++ templates/services.d/azure_metrics_exporter/run.sh
@@ -7,7 +7,7 @@ export AZURE_TENANT_ID="(@= data.values.iaas_config.azure.tenant_id @)"
 export AZURE_CLIENT_ID="(@= data.values.iaas_config.azure.client_id @)"
 export AZURE_CLIENT_SECRET="(@= data.values.iaas_config.azure.client_secret @)"
 
-azure-metrics-exporter
+azure-metrics-exporter --enable-caching
 
 (@ else: @)
 s6-svc -dXO /var/run/s6/services/azure_metrics_exporter
