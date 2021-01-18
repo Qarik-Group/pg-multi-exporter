0c0b59941f9bc9626d3ac2abb21c556068b4ee67
ramonskie <ramonmakkelie@gmail.com>
2021-01-18 15:25:10 +0100

    lunner conditionals for gcp

--- templates/lunner/become_leader.sh
+++ templates/lunner/become_leader.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+(@ load("@ytt:data", "data") @)
 
 echo "BECOMING LEADER"
 
 set -x
 
 s6-svc -u /var/run/s6/services/telegraf
+(@ if hasattr(data.values.iaas_config, "gcp"): @)
 s6-svc -u /var/run/s6/services/telegraf_cpu
 s6-svc -u /var/run/s6/services/telegraf_disk
+(@ end @)
--- templates/lunner/become_standby.sh
+++ templates/lunner/become_standby.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+(@ load("@ytt:data", "data") @)
 
 echo "BECOMING STANDBY"
 
 set -x
 
 s6-svc -dO /var/run/s6/services/telegraf
+(@ if hasattr(data.values.iaas_config, "gcp"): @)
 s6-svc -dO /var/run/s6/services/telegraf_cpu
 s6-svc -dO /var/run/s6/services/telegraf_disk
+(@ end @)
+
